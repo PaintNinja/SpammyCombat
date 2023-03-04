afd1c5f542bbdb0bacb5998d6b7737eb9203a09e
Paint_Ninja <PaintNinja@users.noreply.github.com>
2023-03-04 23:37:48 +0000

    Add item blacklist feature

---
+++ src/main/java/ga/ozli/minecraftmods/spammycombat/Config.java
@@ -0,0 +1,46 @@
+package ga.ozli.minecraftmods.spammycombat;
+
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.common.ForgeConfigSpec;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.config.ModConfigEvent;
+import net.minecraftforge.registries.ForgeRegistries;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Mod.EventBusSubscriber(modid = SpammyCombat.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+final class Config {
+    private static final ForgeConfigSpec.Builder BUILDER = new ForgeConfigSpec.Builder();
+    private static final ForgeConfigSpec.ConfigValue<List<? extends String>> BLACKLIST_STRINGS = BUILDER
+            .comment("A list of items that spam-clicking combat doesn't apply to. Note that sword sweep attacks are still disabled.")
+            .defineListAllowEmpty(Collections.singletonList("blacklist"), Collections::emptyList, Config::validateItemName);
+    static final ForgeConfigSpec SPEC = BUILDER.build();
+
+    private static boolean USE_BLACKLIST;
+    private static Set<Item> BLACKLIST;
+
+    private static boolean validateItemName(final Object obj) {
+        return obj instanceof final String itemName && ForgeRegistries.ITEMS.containsKey(new ResourceLocation(itemName));
+    }
+
+    static boolean canSpamClickWith(final ItemStack itemStack) {
+        return !USE_BLACKLIST || !BLACKLIST.contains(itemStack.getItem());
+    }
+
+    @SubscribeEvent
+    static void onLoad(final ModConfigEvent event) {
+        USE_BLACKLIST = !BLACKLIST_STRINGS.get().isEmpty();
+
+        BLACKLIST = USE_BLACKLIST
+                ? BLACKLIST_STRINGS.get().stream()
+                        .map(itemName -> ForgeRegistries.ITEMS.getValue(new ResourceLocation(itemName)))
+                        .collect(Collectors.toSet())
+                : Collections.emptySet();
+    }
+}
